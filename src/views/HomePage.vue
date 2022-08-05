8f9d2b62cf22aeea626558c3ac1988a382c1127b
FunTonw <qw321888@gmail.com>
2022-08-05 15:49:40 +0800

    add:items

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -154,13 +154,71 @@
               <div>
                 <img src="@/assets/image/items/1_fa5fe15e-75d8-45e0-8156-29ea083372e9.webp" alt="">
               </div>
-              <div class="text-white font-bold text-start">
-                <h5>[定期購買] 燕麥餅乾“FruOats” 5 種套裝 30 件</h5>
-                <p class="text-xl">
-                  ¥2,664
-                  <span class="font-normal text-xs text-gray-200">税込</span>
-                </p>
+                <div class="text-white font-bold text-start mt-3">
+                  <div class="flex items-center">
+                    <h5 class="mr-2 text-sm">【定期購入】オートミールクッキー『FruOats（フルオーツ）』5種セット 30枚入り</h5>
+                    <p class="text-white">></p>
+                  </div>
+                  <p class="text-xl">
+                    ¥2,664
+                    <span class="font-normal text-xs text-gray-200">税込</span>
+                  </p>
+                </div>
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <div>
+                <img src="@/assets/image/items/4629d09ee093272f99b2a6f7d07e9f05_59137082-f8f2-42a7-816e-365bef03f629.webp" alt="">
               </div>
+                <div class="text-white font-bold text-start mt-3">
+                  <div class="flex items-center">
+                    <h5 class="mr-2 text-sm">【定期購入】オートミールクッキー『FruOats（フルオーツ）』アップルシナモン 30枚入り</h5>
+                    <p class="text-white">></p>
+                  </div>
+                  <p class="text-xl">
+                    ¥2,664
+                    <span class="font-normal text-xs text-gray-200">税込</span>
+                  </p>
+                </div>
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <div>
+                <img src="@/assets/image/items/18118f77dc573a6a85cde6f2e14ff456_7056b305-353a-4aad-b219-54ebfc8b19ec.webp" alt="">
+              </div>
+                <div class="text-white font-bold text-start mt-3">
+                  <div class="flex items-center">
+                    <h5 class="mr-2 text-sm">【定期購入】オートミールクッキー『FruOats（フルオーツ）』イチジクアーモンド 30枚入り</h5>
+                    <p class="text-white">></p>
+                  </div>
+                  <p class="text-xl">
+                    ¥2,664
+                    <span class="font-normal text-xs text-gray-200">税込</span>
+                  </p>
+                </div>
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <div>
+                <img src="@/assets/image/items/aded710a6b2d6d460c5c2b814015739b_4ed9fe22-ac87-429e-9e91-57fd82c5951e.webp" alt="">
+              </div>
+                <div class="text-white font-bold text-start mt-3">
+                  <div class="flex items-center">
+                    <h5 class="mr-2 text-sm">【定期購入】オートミールクッキー『FruOats（フルオーツ）』オレンジチョコレート 30枚入り</h5>
+                    <p class="text-white">></p>
+                  </div>
+                  <p class="text-xl">
+                    ¥2,664
+                    <span class="font-normal text-xs text-gray-200">税込</span>
+                  </p>
+                </div>
+
             </a>
           </li>
         </ul>
